3bf64925ee077e4dd27ce28e56a852ea04a2191e
Dhayalan <john@one5commute.com>
2023-08-02 12:56:19 +0530

    ui

--- src/components/ListComponent/List.component.js
+++ src/components/ListComponent/List.component.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const ListComponentContent = (props) => {
   console.log('PRINT MEa ::::', props.list);
+  console.log('DATE ::::', props.date);
   return (
     <Container>
       <Row>
-        <Col className="text-center">
+        <Col sm={8} className="text-center">
           <span className="d-block text-right">Today : {props.date}</span>
           <h1>Book List</h1>
         </Col>
       </Row>
       <Row>
-        <Col className="text-center">
+        <Col sm={4} className="text-center">
           <input className="form-control" type="text" />
         </Col>
       </Row>
